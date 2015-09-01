9b72918beddb7a4f5ad13c237346f4c33c101b9c
jogag <jogag@users.noreply.github.com>
2015-09-01 08:36:02 +0200

    Fix a bug in keyboard
    Fixed a bug that sends nil (=does not send) instead of the text
    (mistype: "text" instead of "fields.text")

--- digiboard/init.lua
+++ digiboard/init.lua
@@ -47,7 +47,7 @@ minetest.register_node("digiboard:keyboard", {
       meta:set_string("channel")
       meta:set_string("formspec", "field[text;Enter text;]")
     elseif fields.text then
-      digiline:receptor_send(pos, digiline.rules.default, channel, text)
+      digiline:receptor_send(pos, digiline.rules.default, channel, fields.text)
     end
   end,
 })
